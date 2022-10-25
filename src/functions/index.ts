aa7846aab49e20e4bf03faa2402cdc6bff59e73b
SaudC21 <saudalslimany@gmail.com>
2022-10-26 00:45:06 +0300

    Update index.ts

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,33 +1,34 @@
 import sharp from 'sharp';
 import fs from 'fs';
-import { resolve } from 'path';
-const rootDir = resolve('./');
+import path from 'path';
+const rootDir = path.resolve('./');
 
 const resizeImg = async (
   imgName: string,
   height: number,
   width: number
 ): Promise<string> => {
+  const newImage = path.join(rootDir, 'output', imgName + '-' + height + '-' + width + '-output.jpg')
   if (checkImage(imgName) == false) {
     return 'This image is not available, Please enter correct image name';
   } else if (checkFile(imgName, height, width) == true) {
-    return rootDir + `\\output\\${imgName}-${height}-${width}-output.jpg`;
+    return newImage
   } else {
-    await sharp(rootDir + '\\Images\\' + imgName + '.jpg')
+    await sharp(path.join(rootDir, 'Images', imgName + '.jpg'))
       .resize(width, height)
-      .toFile(`${rootDir}\\output\\${imgName}-${height}-${width}-output.jpg`);
-    return `${rootDir}\\output\\${imgName}-${height}-${width}-output.jpg`;
+      .toFile(newImage);
+    return newImage;
   }
 };
 
 const checkImage = (imgName: string): boolean => {
-  const folder = `${rootDir}\\Images\\`;
-  return fs.existsSync(`${folder}${imgName}.jpg`);
+  // To check images from Images folder
+  return fs.existsSync(path.join(rootDir, 'Images', imgName + '.jpg'));
 };
 
 const checkFile = (imgName: string, height: number, width: number): boolean => {
-  const folder = `${rootDir}\\output\\`;
-  return fs.existsSync(`${folder}${imgName}-${height}-${width}-output.jpg`);
+  // To check images from output folder
+  return fs.existsSync(path.join(rootDir, 'Images', imgName + '-' + height + '-' + width + '-output.jpg'));
 };
 
 export default resizeImg;
